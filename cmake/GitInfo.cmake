c6cec9c14209cfe2fc1797379187956ba2080fb6
Bruk Tedla <biruk.tedla93@gmail.com>
2021-11-30 17:49:54 +0300

    format cmake files

--- cmake/GitInfo.cmake
+++ cmake/GitInfo.cmake
@@ -1,12 +1,10 @@
 # Copyright (c) 2011-2014 Stefan Eilemann <eile@eyescale.ch>
 
-# Sets the following variables if git is found:
-# GIT_REVISION: The current HEAD sha hash
-# GIT_STATE: A description of the working tree, e.g., 1.8.0-48-g6d23f80-dirty
-# GIT_ORIGIN_URL: The origin of the working tree
-# GIT_ROOT_URL: The root remote of the working tree
-# GIT_BRANCH: The name of the current branch
-# GIT_AUTHORS: A list of all authors in the git history
+# Sets the following variables if git is found: GIT_REVISION: The current HEAD
+# sha hash GIT_STATE: A description of the working tree, e.g.,
+# 1.8.0-48-g6d23f80-dirty GIT_ORIGIN_URL: The origin of the working tree
+# GIT_ROOT_URL: The root remote of the working tree GIT_BRANCH: The name of the
+# current branch GIT_AUTHORS: A list of all authors in the git history
 
 if(GIT_INFO_DONE_${PROJECT_NAME})
   return()
@@ -23,20 +21,30 @@ if(EXISTS ${PROJECT_SOURCE_DIR}/.git)
     find_package(Git QUIET)
   endif()
   if(GIT_FOUND)
-    execute_process( COMMAND "${GIT_EXECUTABLE}" rev-parse --short HEAD
+    execute_process(
+      COMMAND "${GIT_EXECUTABLE}" rev-parse --short HEAD
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-      OUTPUT_VARIABLE GIT_REVISION OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process( COMMAND "${GIT_EXECUTABLE}" describe --long --tags --dirty --always
+      OUTPUT_VARIABLE GIT_REVISION
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process(
+      COMMAND "${GIT_EXECUTABLE}" describe --long --tags --dirty --always
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-      OUTPUT_VARIABLE GIT_STATE OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
-    execute_process( COMMAND "${GIT_EXECUTABLE}" config --get remote.origin.url
-      OUTPUT_VARIABLE GIT_ORIGIN_URL OUTPUT_STRIP_TRAILING_WHITESPACE
+      OUTPUT_VARIABLE GIT_STATE
+      OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+    execute_process(
+      COMMAND "${GIT_EXECUTABLE}" config --get remote.origin.url
+      OUTPUT_VARIABLE GIT_ORIGIN_URL
+      OUTPUT_STRIP_TRAILING_WHITESPACE
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
-    execute_process( COMMAND "${GIT_EXECUTABLE}" config --get remote.root.url
-      OUTPUT_VARIABLE GIT_ROOT_URL OUTPUT_STRIP_TRAILING_WHITESPACE
+    execute_process(
+      COMMAND "${GIT_EXECUTABLE}" config --get remote.root.url
+      OUTPUT_VARIABLE GIT_ROOT_URL
+      OUTPUT_STRIP_TRAILING_WHITESPACE
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
-    execute_process( COMMAND "${GIT_EXECUTABLE}" branch --contains HEAD
-      OUTPUT_VARIABLE GIT_BRANCH OUTPUT_STRIP_TRAILING_WHITESPACE
+    execute_process(
+      COMMAND "${GIT_EXECUTABLE}" branch --contains HEAD
+      OUTPUT_VARIABLE GIT_BRANCH
+      OUTPUT_STRIP_TRAILING_WHITESPACE
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
 
     if(EXISTS ${PROJECT_BINARY_DIR}/Authors.txt)
@@ -44,8 +52,10 @@ if(EXISTS ${PROJECT_SOURCE_DIR}/.git)
     else()
       # cache authors to not slow down cmake runs
       set(GIT_AUTHORS)
-      execute_process(COMMAND "${GIT_EXECUTABLE}" log
-        OUTPUT_VARIABLE GIT_LOG OUTPUT_STRIP_TRAILING_WHITESPACE
+      execute_process(
+        COMMAND "${GIT_EXECUTABLE}" log
+        OUTPUT_VARIABLE GIT_LOG
+        OUTPUT_STRIP_TRAILING_WHITESPACE
         WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
       string(REPLACE "\n" ";" GIT_LOG ${GIT_LOG})
       foreach(__line ${GIT_LOG})
